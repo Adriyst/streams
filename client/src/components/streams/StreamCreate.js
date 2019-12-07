e61ef8cbc9d99b50ed9e20ee91bca75885d466b9
Adrian Tysnes <adriantysnes@localhost.localdomain>
2019-12-07 23:55:15 +0100

    new action creater, removed db from git

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
@@ -14,8 +16,8 @@ class StreamCreate extends React.Component {
         );
     }
 
-    onSubmit(formValues) {
-        console.log(formValues);
+    onSubmit = formValues => {
+        this.props.createStream(formValues);
     } 
 
 
@@ -37,7 +39,9 @@ const validate = formValues => {
     return err_obj;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: "streamCreate",
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
