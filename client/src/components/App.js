e4b88854243c4026ab39331e64f90624554a1596
Adrian Tysnes <adriantysnes@localhost.localdomain>
2019-12-09 15:05:54 +0100

    changed router, added history

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import Header from './Header';
+import history from '../history';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -13,14 +14,14 @@ import StreamShow from './streams/StreamShow';
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <Header />
                 <Route path="/" exact component={StreamList}/>
                 <Route path="/streams/new" exact component={StreamCreate}/>
                 <Route path="/streams/edit" exact component={StreamEdit}/>
                 <Route path="/streams/delete" exact component={StreamDelete}/>
                 <Route path="/streams/show" exact component={StreamShow}/>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
